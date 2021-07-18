5933332c039b38ec3527534ce0c3f5d09eb337a8
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-07-18 09:21:30 -0700

    add website

---
+++ app/README.md
@@ -0,0 +1,6 @@
+# Vampire Tokens - Taken from Svelte Tezos Template
+
+## A Svelte template to build awesome dapps on Tezos!
+
+1. `npm install`
+2. `npm run dev`
---
+++ app/src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+
+</script>
+
+<style lang="scss">
+  $tezos-blue: #5d0808;
+
+
+  .title {
+    color: $tezos-blue;
+    font-size: 100px;
+    margin: 20px;
+  }
+
+  .subtitle {
+    font-size: 40px;
+    color: #5d0808;
+    margin: 10px;
+  }
+
+  .link {
+    font-size: 20px;
+    color: #5d0808;
+    margin: 10px;
+  }
+
+</style>
+
+<main>
+    <div class="title">Vampire Tokens</div>
+    <img src={`images/vampire.png`} alt={'vampire'} style="width:100px"/>
+    <div class="subtitle">Hunt, Stake or Burn!</div>
+
+    <div class="link"><a href="https://quipuswap.com/swap?from=tez&to=KT1QQPYLhqHSkJQNYdLQuNAw5uYkQbJ1s2Jw_0">Hunt Vampires at QuipuSwap</a><br>
+    <div class="link"><a href="https://quipuswap.com/invest/add-liquidity/KT1QQPYLhqHSkJQNYdLQuNAw5uYkQbJ1s2Jw_0">Stake Vampires by adding to the liquidity pool at QuipuSwap</a></div>
+
+  <div class="link">To burn Vampires send them to burner.crunchy.tez</div>
+
+
+  <div>
+</main>
