790f25ca400793b1f2d994d371dd4c67f796f66c
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2021-07-19 12:51:42 -0700

    changes to webpage again

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -37,14 +37,14 @@
 <main>
     <div class="title">Vampire Tokens</div>
     <img src={`images/vampire.png`} alt={'vampire'} style="width:100px"/>
-    <div class="subtitle">You must hunt them and stake or burn them!</div>
+    <div class="subtitle">You must hunt them and stake them or burn them!</div>
 
     <div class="link">Hunt Vampires at <a href="https://quipuswap.com/swap?from=tez&to=KT1QQPYLhqHSkJQNYdLQuNAw5uYkQbJ1s2Jw_0">QuipuSwap</a><br>
     <div class="link">Stake Vampires by adding to the liquidity pool at <a href="https://quipuswap.com/invest/add-liquidity/KT1QQPYLhqHSkJQNYdLQuNAw5uYkQbJ1s2Jw_0">QuipuSwap</a></div>
 
-  <div class="link">Burn* Vampires by sending them to KT1CZMurPAjSfZqcn6LBUNUhG4byE6AJgDT6</div>
+  <div class="link">Burn<sup>&dagger</sup> Vampires by sending them to KT1CZMurPAjSfZqcn6LBUNUhG4byE6AJgDT6</div>
 
-  <div class="note">* Vampire's burn when exposed to sunlight or doused with Holy Water.</div>
+  <div class="note">&dagger Vampires burn when exposed to sunlight or doused with Holy Water.</div>
 
 
   <div>
